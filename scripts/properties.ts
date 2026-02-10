a65be7ceb989d080258d6176392b12c6f94746ff
shizuku86 <shizuku86710@gmail.com>
2026-02-10 18:42:42 +0900

    fixed properties

--- scripts/properties.ts
+++ scripts/properties.ts
@@ -1,27 +1,8 @@
-/**
- * scripts/properties から manifest.jsonを自動生成する
- * propertiesは、アドオン間通信においても、識別などに利用する
- */
-
 import type { KairoAddonProperties } from "@kairo-ts/router";
 
-export type SemVer = {
-    major: number;
-    minor: number;
-    patch: number;
-    prerelease?: string | undefined; // "preview.3" / "rc.1"
-    build?: string | undefined; // "abc123" (commit)
-};
-
-/**
- * 文末に # が記述されている箇所を適宜修正して使用します。
- * Modify and use where # is written at the end of the sentence as appropriate
- */
-
 export const properties: KairoAddonProperties = {
-    id: "werewolf-additionalroles-1", // a-z & 0-9 - _
+    id: "werewolf-additionalroles-1", //# // a-z & 0-9 - _
     metadata: {
-        /** 製作者の名前 */
         authors: ["shizuku86"],
     },
     header: {
@@ -31,27 +12,11 @@ export const properties: KairoAddonProperties = {
             major: 1,
             minor: 0,
             patch: 0,
-            // prerelease: "preview.1",
+            prerelease: "dev.1",
             // build: "abc123",
         },
-        min_engine_version: [1, 21, 100],
-        uuid: "91f2e238-35ca-4d35-a594-685aa2f24f8e",
+        min_engine_version: [1, 21, 132],
     },
-    resourcepack: {
-        name: "Use BP Name",
-        description: "Use BP Description",
-        uuid: "6d77d14e-49ee-4e8c-b7ed-ad5acf1d445a",
-        module_uuid: "a7854fd8-5549-451e-8db2-820d0a8096b9",
-    },
-    modules: [
-        {
-            type: "script",
-            language: "javascript",
-            entry: "scripts/index.js",
-            version: "header.version",
-            uuid: "8146f24e-2906-449c-8d1b-63996a7e32fb",
-        },
-    ],
     dependencies: [
         {
             module_name: "@minecraft/server",
@@ -64,16 +29,10 @@ export const properties: KairoAddonProperties = {
     ],
     /** 前提アドオン */
     requiredAddons: {
+        /**
+         * id: version (string) // "kairo": "1.0.0"
+         */
         "werewolf-gamemanager": "1.0.0-dev.1",
     },
-    tags: ["stable"],
+    tags: ["official", "stable"],
 };
-
-/**
- * "official" を非公式に付与することは許可されていません。
- * 公認のアドオンには "approved" を付与します。
- * It is not allowed to assign "official" unofficially.
- * For approved addons, assign "approved".
- *
- */
-export const supportedTags: string[] = ["official", "approved", "stable", "experimental"];
